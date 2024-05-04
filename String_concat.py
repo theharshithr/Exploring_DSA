9426a2f2d5149932366282cbaee5bf5e265054e7
Harshith R <55926625+theharshithr@users.noreply.github.com>
2024-05-05 02:35:56 +0530

    update String_concat.py
    Co-Authored-By: Harshith R <55926625+theharshithr@users.noreply.github.com>

--- String_concat.py
+++ String_concat.py
@@ -1,13 +1,1 @@
-def main():
-    init = "This is concatinating"
-    add = " 2 strings together"
-
-    # Concatenate strings
-    result = init + add
-
-    # Print the result
-    print(result)
-
-# Call the main function
-if __name__ == "__main__":
-    main()
+const pair = "extraordinaire";
