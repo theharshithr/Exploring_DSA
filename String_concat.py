e7ac5eb191287df990a8f5b7aeed7ddfb8e22845
Harshith R <55926625+theharshithr@users.noreply.github.com>
2024-05-05 01:37:08 +0530

    String concatenation.

---
+++ String_concat.py
@@ -0,0 +1,13 @@
+def main():
+    init = "This is concatinating"
+    add = " 2 strings together"
+
+    # Concatenate strings
+    result = init + add
+
+    # Print the result
+    print(result)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
